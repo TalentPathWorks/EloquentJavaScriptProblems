087717605953e89c850cf3f115132c7a95fe11d8
Roberto Sanchez <rsanchez92@live.com>
2019-07-22 12:35:11 -0700

    Stumped on 5-4

--- Chapter05.js
+++ Chapter05.js
@@ -64,8 +64,13 @@
 function every(array, test) {
   if(array.length === 0)return true;
   
-  return newArray = array.map(test).reduce((accumulator,currentValue)=>currentValue && accumulator)
+  return array.map(test).reduce((accumulator,currentValue)=> currentValue && accumulator)
 };
+// Not functional, needs more work
+function every2(array,test){
+  if(array.length === 0) return true;
+  return array.map(test).some((element)=>{return element === true});
+}
 console.log(`5-3: Everything`)
 console.log(every([1, 3, 5], n => n < 10));
 // → true
@@ -83,11 +88,49 @@ console.log(every([], n => n < 10));
  * have a script associated with them. The characterScript and countBy functions
  * defined earlier in the chapter are probably useful here.
  */
+const SCRIPTS = require('./Scripts.js')
 
-function dominantDirection(text) {
-  // Your code here.
+function characterScript(code) {
+  for (let script of SCRIPTS) {
+    if (script.ranges.some(([from, to]) => {
+      return code >= from && code < to;
+    })) {
+      return script;
+    }
+  }
+  return null;
+}
+function countBy(items, groupName) {
+  let counts = [];
+  for (let item of items) {
+    let name = groupName(item);
+    let known = counts.findIndex(c => c.name == name);
+    if (known == -1) {
+      counts.push({name, count: 1});
+    } else {
+      counts[known].count++;
+    }
+  }
+  return counts;
 }
 
+function textScripts(text) {
+  let scripts = countBy(text, char => {
+    let script = characterScript(char.codePointAt(0));
+    return script ? script.name : "none";
+  }).filter(({name}) => name != "none");
+
+  let total = scripts.reduce((n, {count}) => n + count, 0);
+  if (total == 0) return "No scripts found";
+
+  return scripts.map(({name, count}) => {
+    return `${Math.round(count * 100 / total)}% ${name}`;
+  }).join(", ");
+}
+function dominantDirection(text) {
+  return characterScript(text.codePointAt(0))
+}
+console.log(`5-4: Dominant Writting Direction `)
 console.log(dominantDirection("Hello!"));
 // → ltr
 console.log(dominantDirection("Hey, مساء الخير"));
